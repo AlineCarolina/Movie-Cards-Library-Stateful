41b14262a2e7c2c6d2a3f94143da87bd3881cd59
Aline <eualine2017@gmail.com>
2022-08-03 20:18:37 -0300

    feat: refactoring details

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,3 @@
-// implement SearchBar component here
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -14,36 +13,36 @@ class SearchBar extends React.Component {
     } = this.props;
 
     return (
-      <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="searchText">
+      <form>
+        <label htmlFor="searchText">
           Inclui o texto:
           <input
-            data-testid="text-input"
             type="text"
             value={ searchText }
             onChange={ onSearchTextChange }
+            name="searchText"
           />
         </label>
-        <label data-testid="checkbox-input-label" htmlFor="bookmarked">
+        <label htmlFor="bookmarked">
           Mostrar somente favoritos
           <input
             type="checkbox"
             checked={ bookmarkedOnly }
             onChange={ onBookmarkedChange }
-            data-testid="checkbox-input"
+            name="bookmarkedOnly"
           />
         </label>
-        <label data-testid="select-input-label" htmlFor="filter">
+        <label htmlFor="filter">
           Filtrar por gênero
           <select
             value={ selectedGenre }
             onChange={ onSelectedGenreChange }
-            data-testid="select-input"
+            name="selectedGenre"
           >
-            <option data-testid="select-option" value="">Todos</option>
-            <option data-testid="select-option" value="action">Ação</option>
-            <option data-testid="select-option" value="comedy">Comédia</option>
-            <option data-testid="select-option" value="thriller">Suspense</option>
+            <option value="">Todos</option>
+            <option value="action">Ação</option>
+            <option value="comedy">Comédia</option>
+            <option value="thriller">Suspense</option>
           </select>
         </label>
       </form>
