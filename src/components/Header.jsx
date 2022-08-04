52060295805278d212112918b0487268101382e9
Aline <eualine2017@gmail.com>
2022-08-03 22:27:33 -0300

    feat: refactoring and adding css to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/Header.css'
 
 class Header extends React.Component {
   render() {
     return (
       <header className="movie-card-header">
         <h1 className="page-title">
-          Movie Cards Library
+          Movie Cards Library Stateful
         </h1>
       </header>
     );
